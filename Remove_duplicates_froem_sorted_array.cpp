5c274127d3fdb40b1ca726abded2d093623a620d
mohammedGasser <mohamedgassermohamede@gmail.com>
2025-03-18 05:06:53 +0200

    Update Remove_duplicates_froem_sorted_array.cpp

--- Remove_duplicates_froem_sorted_array.cpp
+++ Remove_duplicates_froem_sorted_array.cpp
@@ -13,6 +13,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// the solution of question
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+        
+//            int index=1; 
+
+//             for(int i=0; i<nums.size()-1;i++)
+//             {   
+//                    if(nums[i] != nums[i+1])
+//                    nums[index++]=nums[i+1];
+//             }
+
+
+
+//         return index;
+//     }
+    
+// };
+
+
+
+
 int flag=0;
 int main() {
 
@@ -103,6 +127,12 @@ int main() {
 	//	{
 	//		cout<<o;
 	//	}
+
+
+
+
+
+	
 	return 0;
 }
 
